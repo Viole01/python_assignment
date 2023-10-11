6e66540cb8c16be0b75053c59732c94362b15d68
Prajjwal <prajjwalpawar1@gmail.com>
2023-10-11 13:03:00 +0530

    First Question Done

--- python_assignment_1.py
+++ python_assignment_1.py
@@ -1,20 +1,36 @@
+import re
+
 # Q1. In DevOps, security is a crucial aspect, and ensuring strong passwords is essential. Create a Python script to check the password strength.
 passwd = input("Please enter your password: ")
 
 
 def check_password_strength(passwd):
-    res = False
+    # Checking the length
     if len(passwd) < 8:
-        res = True
-        return res
-    for char in passwd:
-        if char.isupper():
-            res = True
-            return res
-    for char in passwd:
-        if char.islower():
-            res = True
-            return res
+        return False
+    # Checking Uppercase
+    if not any(char.isupper() for char in passwd):
+        return False
+    # Checking Lowercase
+    if not any(char.islower() for char in passwd):
+        return False
+    # Checking Number
+    if not any(char.isdigit() for char in passwd):
+        return False
+    # Special Character Check
+    special_chars = r"[!@#$%^&*(),.?\":{}|<>]"
+    if not re.search(special_chars, passwd):
+        return False
+
+    return True
 
 
-check_password_strength(passwd)
+def provide_feedback(passwd):
+    if check_password_strength(passwd):
+        return "Your Password is strong!"
+    else:
+        return "Please check your password, make sure it meets all the criteria."
+
+
+# Calling the function and printing the output
+print(provide_feedback(passwd))
