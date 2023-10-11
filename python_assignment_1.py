c7bab84d01f604c96de13d359393273cb19be390
Prajjwal <prajjwalpawar1@gmail.com>
2023-10-11 12:22:30 +0530

    First question done

--- python_assignment_1.py
+++ python_assignment_1.py
@@ -1,20 +1,39 @@
+import re  # Using regular expression for pattern matching in strings
+
 # Q1. In DevOps, security is a crucial aspect, and ensuring strong passwords is essential. Create a Python script to check the password strength.
 passwd = input("Please enter your password: ")
 
 
 def check_password_strength(passwd):
-    res = False
+    # Minimum length check
     if len(passwd) < 8:
-        res = True
-        return res
+        return False
+    # Uppercase Check
     for char in passwd:
-        if char.isupper():
-            res = True
-            return res
+        if not char.isupper():
+            return False
+    # Lowercase Check
     for char in passwd:
-        if char.islower():
-            res = True
-            return res
+        if not char.islower():
+            return False
+    # Digit Check
+    for char in passwd:
+        if not char.isdigit():
+            return False
+    # Special Character Check
+    special_chars = r"[!@#$%^&*(),.?\":{}|<>]"
+    if not re.search(special_chars, passwd):
+        return False
+
+    return True
 
 
-check_password_strength(passwd)
+def provide_feedback(passwd):
+    if check_password_strength(passwd):
+        return "Your Password is strong!"
+    else:
+        return "Please check your password, make sure it meets all the criteria."
+
+
+# Callinf the function and printing the output
+print(provide_feedback(passwd))
