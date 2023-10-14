856b6fea613cea860db6bee5a1a81aa289545b5a
Prajjwal <prajjwalpawar1@gmail.com>
2023-10-14 17:00:12 +0530

    Revert "First question done"
    This reverts commit c7bab84d01f604c96de13d359393273cb19be390.
    
    "Reverting my commit"

--- python_assignment_1.py
+++ python_assignment_1.py
@@ -1,39 +1,20 @@
-import re  # Using regular expression for pattern matching in strings
-
 # Q1. In DevOps, security is a crucial aspect, and ensuring strong passwords is essential. Create a Python script to check the password strength.
 passwd = input("Please enter your password: ")
 
 
 def check_password_strength(passwd):
-    # Minimum length check
+    res = False
     if len(passwd) < 8:
-        return False
-    # Uppercase Check
+        res = True
+        return res
     for char in passwd:
-        if not char.isupper():
-            return False
-    # Lowercase Check
+        if char.isupper():
+            res = True
+            return res
     for char in passwd:
-        if not char.islower():
-            return False
-    # Digit Check
-    for char in passwd:
-        if not char.isdigit():
-            return False
-    # Special Character Check
-    special_chars = r"[!@#$%^&*(),.?\":{}|<>]"
-    if not re.search(special_chars, passwd):
-        return False
-
-    return True
+        if char.islower():
+            res = True
+            return res
 
 
-def provide_feedback(passwd):
-    if check_password_strength(passwd):
-        return "Your Password is strong!"
-    else:
-        return "Please check your password, make sure it meets all the criteria."
-
-
-# Callinf the function and printing the output
-print(provide_feedback(passwd))
+check_password_strength(passwd)
